9be7d0da4d596feee3dab99bcefd06148ea27023
Jibran-Khot <jibrankhot37@gmail.com>
2026-05-19 22:06:17 +0530

    refactor: migrate frontend to standalone scalable architecture

--- src/app/shared/pipes/status-label.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StatusLabelPipe } from './status-label.pipe';
-
-describe('StatusLabelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StatusLabelPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
